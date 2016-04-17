1dd6515f5f6f5e269c0ce8cd15186f3b7464abce
afonsopacifer <afonsopolis@gmail.com>
2016-04-17 02:38:59 -0300

    Fisrt section visual refactor done and code refactor started

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,7 @@
-console.log("test");
-
 (function() {
   var btnOpen = document.getElementById("navOpen");
   var menu = document.getElementById("navigation");
 
-  //Função para exibir ou ocultar os itens do menu
   function toggleMenu() {
     if(menu.style.display === "none") {
       menu.style.display = "block";
@@ -13,20 +10,16 @@ console.log("test");
     }
   }
 
-  //Atribui a função ao evento click no botão abrir
   btnOpen.addEventListener("click",toggleMenu);
 
-  //Cria a media querie
-  var mediaQuery = window.matchMedia("(min-width: 768px)");
+  var mediaQuery = window.matchMedia("(min-width: 840px)");
 
-  //Verifica a media query e determina se os itens do menu deve aparecer ou não
   if (mediaQuery.matches) {
     menu.style.display = "block";
   } else {
     menu.style.display = "none";
   }
 
-  //Verifica alterações e e determina se os itens do menu deve aparecer ou não
   mediaQuery.addListener(function(changed) {
     if(changed.matches) {
       menu.style.display = "block";
